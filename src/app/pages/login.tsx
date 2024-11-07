c02ca09a8095e5457c1f572a972f91e8b7691d0c
kayla2615 <76797642+kayla2615@users.noreply.github.com>
2024-11-07 18:55:19 -0500

    Create login.tsx

---
+++ src/app/pages/login.tsx
@@ -0,0 +1,14 @@
+// pages/login.tsx
+
+import React from 'react';
+import LoginForm from '../components/LoginPage';
+
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
