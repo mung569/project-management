68a92c0fe7005a4b606da7d961230ae493b7a1db
muacm750 <112666606+muacm750@users.noreply.github.com>
2024-11-10 12:25:30 -0500

    Added home icon to Signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import styles from './Signup.module.css'; 
 import Link from 'next/link';
+import { IoMdHome } from "react-icons/io";
+
 
 const LoginForm: React.FC = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -10,36 +12,41 @@ const LoginForm: React.FC = () => {
   };
 
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.register}>Register for Swift</h1>
-      <form onSubmit={handleSubmit}>
-        <h1 className={styles.signup}>Sign Up</h1>
-        
-        <div className={styles.inputBox}>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" placeholder="Enter your email" required />
-          <i className='bx bx-user'></i>
-        </div>
+    <div>
+      <nav className={styles.nav}>
+        <Link href='./'><IoMdHome className={styles.homeIcon} /></Link>
+      </nav>
+      <div className={styles.wrapper}>
+        <h1 className={styles.register}>Register for Swift</h1>
+        <form onSubmit={handleSubmit}>
+          <h1 className={styles.signup}>Sign Up</h1>
+          
+          <div className={styles.inputBox}>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" placeholder="Enter your email" required />
+            <i className='bx bx-user'></i>
+          </div>
 
-        
-        <div className={styles.inputBox}>
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" placeholder="Enter a username" required />
-          <i className='bx bx-user'></i>
-        </div>
+          
+          <div className={styles.inputBox}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" placeholder="Enter a username" required />
+            <i className='bx bx-user'></i>
+          </div>
 
-        
-        <div className={styles.inputBox}>
-          <label htmlFor="password">Password</label>
-          <input  id="password" type="password" placeholder="Enter your password" required />
-          <i className='bx bxs-lock-alt'></i>
-        </div>
+          
+          <div className={styles.inputBox}>
+            <label htmlFor="password">Password</label>
+            <input  id="password" type="password" placeholder="Enter your password" required />
+            <i className='bx bxs-lock-alt'></i>
+          </div>
 
-        <button type="submit" className={styles.btn}>Register</button>
-      </form>
-      <div className={styles.registerLink}>
-          <p>Already have an account? <Link href="/login">Log In</Link></p>
-        </div>
+          <button type="submit" className={styles.btn}>Register</button>
+        </form>
+        <div className={styles.registerLink}>
+            <p>Already have an account? <Link href="/login">Log In</Link></p>
+          </div>
+      </div>
     </div>
   );
 };
